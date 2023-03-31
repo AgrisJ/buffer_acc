42ae24fdd6af0dba425ecbb5ca40a4135efeeb2d
AgrisJ <agris.jurjans@gmail.com>
2023-03-31 07:53:12 +0200

    Updated balance intro april

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function stringifyElmHTML(element) {
 ///////////////////////////////////////////////////////////////////////////////////
 // @moneyLeftFromLastMonth = what's left from last month after all current month payments;
 
-const moneyLeftFromLastMonth = 5348; // before 11908
+const moneyLeftFromLastMonth = 6550; // before 11908
 //////////////////////////////////////////////////////////////////////////////////
 
 renderApp();
