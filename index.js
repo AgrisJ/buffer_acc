07f3d28fe3733a170b5a5cf18ae7a4d1c95d5a6a
AgrisJ <agris.jurjans@gmail.com>
2023-05-31 08:44:54 +0200

    Updated june balance

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function stringifyElmHTML(element) {
 ///////////////////////////////////////////////////////////////////////////////////
 // @moneyLeftFromLastMonth = what's left from last month after all current month payments;
 
-const moneyLeftFromLastMonth = 6856; // before 11908
+const moneyLeftFromLastMonth = 7499;
 //////////////////////////////////////////////////////////////////////////////////
 
 renderApp();
