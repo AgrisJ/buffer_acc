9061fec5e46e311d69365b9d1b8c0e093e1afb75
AgrisJ <agris.jurjans@gmail.com>
2025-03-01 12:18:31 +0100

    Updated March and 2025 changes

--- cost_variables.js
+++ cost_variables.js
@@ -1,20 +1,20 @@
 // OUTCOME
 const // DAGINSTITUTION COSTS
   // VUGGESTUE_VALTER_COST = -4562, //+700 2023-Madordering
-  BORNEHAVE_VALTER_COST = -3455, // Since 2024-04
+  BORNEHAVE_VALTER_COST = -3521, // Since 2024-04 3455
   //BORNEHAVE_OLIVER_COST = -2471,
   CLUB_ACTIVITY_OLIVER_COST = -1100,
   CLUB_ACTIVITY_VALTER_COST = -500,
   // BORNEHAVE_OLIVER_WITH_VALTER_COST = -1597, //+250 2023-Madordering
-  SFO_OLIVER_COST = -1076, // Since 2024-04
+  SFO_OLIVER_COST = -1117, // 1075 since 2024-04
   // HOUSE COSTS
-  EJENDOMMENSKAT_COST = -1806, // -3150 2020 // -4020 2023
+  EJENDOMMENSKAT_COST = -3918, // -3150 2020 // -4020 2023 // was twice prY -1806
   VANDVÆRK_COST = -1190, // -1450 2023
-  VARMEVÆRK_COST = -3070, // was 920 every month // 5800kr new price from 2023 // later -3872
-  SPILDEVAND_COST = -3256, // -3287 year 2021/1 // -2507 2023
-  RENOVATION_COST = -470,
-  EL_COST = -1800 // -400: before 31-07-2023 // from 09/2024 including also clever
-  // EL_CAR_EL_COST = -1200; // -1500:  before 31-5-2023
+  VARMEVÆRK_COST = -3670, // was 920 every month // 5800kr new price from 2023 // later -3872 // -3070 2025
+  SPILDEVAND_COST = -4100, // -3287 year 2021/1 // -2507 2023 // -3256 2025
+  // RENOVATION_COST = -470,
+  EL_COST = -1800; // -400: before 31-07-2023 // from 09/2024 including also clever
+// EL_CAR_EL_COST = -1200; // -1500:  before 31-5-2023
 
 const // INCOME
   BORNEPENGE = 4168 + 4168, // 9258,
@@ -32,6 +32,18 @@ const // INCOME
     11483 - 6068 = 5415kr (total el usage minus clever)
   - Changed TOBUFFER from 7400 to 8680
 */
+
+/**
+ * DATE: 01-03-2025
+ * - Ejendommenskat has moved from Jan to Mar
+ * - Vandværk has moved from Feb to Mar
+ * - Spildevand increasing price to min 4100kr
+ * - VARMEVÆRK_COST increasing price to min 3670kr
+ * - SFO_OLIVER_COST increasing price to min 1117kr
+ * - BORNEHAVE_VALTER_COST increasing price to min 3521kr
+ * - Ejendommenskat will be payable only once a year - Mar // 3918kr
+ * - Renovation is removed - hasn't been there for more than a year
+ */
 
 export {
   VUGGESTUE_VALTER_COST,
--- data.js
+++ data.js
@@ -6,7 +6,7 @@ export const data = [
       {
         SFO: vari.SFO_OLIVER_COST,
         Bornehave: vari.BORNEHAVE_VALTER_COST,
-        Ejendommenskat: vari.EJENDOMMENSKAT_COST,
+        // Ejendommenskat: vari.EJENDOMMENSKAT_COST,
         Bornepenge: vari.BORNEPENGE,
         Electricity: vari.EL_COST,
         FromFood: vari.TOBUFFER,
@@ -19,8 +19,8 @@ export const data = [
       {
         SFO: vari.SFO_OLIVER_COST,
         Bornehave: vari.BORNEHAVE_VALTER_COST,
-        Vandværk: vari.VANDVÆRK_COST,
-        Varmeværk: vari.VARMEVÆRK_COST,
+        // Vandværk: vari.VANDVÆRK_COST,
+        Varmeværk: vari.VARMEVÆRK_COST, // actual 3670 at 2025
         Electricity: vari.EL_COST,
         FromFood: vari.TOBUFFER,
       },
@@ -34,10 +34,13 @@ export const data = [
         Bornehave: vari.BORNEHAVE_VALTER_COST,
         //Spildevand: -4000, // year 2019
         Spildevand: vari.SPILDEVAND_COST,
+        // Spildevand: -4999, // actual cost : 01-03-2025
         // Spildevand: -4692, // actual cost : 23-02-2023
-        Renovation: vari.RENOVATION_COST,
+        Ejendommenskat: vari.EJENDOMMENSKAT_COST, // from Feb to March at 2025 // -3918 actual cost : 01-03-2025
+        // Renovation: vari.RENOVATION_COST,
         Electricity: vari.EL_COST,
         // Electricity: -3766, // actual cost : 23-02-2023
+        Vandværk: vari.VANDVÆRK_COST, // from Feb to March at 2025 // actual 1530 at 2025
         FromFood: vari.TOBUFFER,
       },
     ],
@@ -83,7 +86,7 @@ export const data = [
     Month: 'July',
     Money: [
       {
-        Ejendommenskat: vari.EJENDOMMENSKAT_COST,
+        // Ejendommenskat: vari.EJENDOMMENSKAT_COST,
         Bornepenge: vari.BORNEPENGE,
         Electricity: vari.EL_COST,
         FromFood: vari.TOBUFFER,
@@ -112,7 +115,7 @@ export const data = [
         ClubActivityOliver: vari.CLUB_ACTIVITY_OLIVER_COST,
         ClubActivityValter: vari.CLUB_ACTIVITY_VALTER_COST,
         Spildevand: vari.SPILDEVAND_COST,
-        Renovation: vari.RENOVATION_COST,
+        // Renovation: vari.RENOVATION_COST,
         Electricity: vari.EL_COST,
         FromFood: vari.TOBUFFER,
       },
